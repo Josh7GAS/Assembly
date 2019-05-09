9d43f7fd441a77044131c849ccdd24e376b8da44
Josh7GAS <joshuaandre@gmail.com>
2019-05-09 19:29:28 -0300

    newhello, exercicio de low

--- Assembly/basics/newhello.asm
+++ Assembly/basics/newhello.asm
@@ -1,15 +1,16 @@
-segment .text  ; code segment
-    global_start        ;most be declared for linker 
-_start: ;tell linker a entry point 
-mov edx,len ;message length
-mov ecx,msg ;message to write
-mov ebx,1 ; file descriptor (stdout)
-mov eax,4 ; system call number 
-int 0x80 ;call kernel
+section	.text
+   global _start     ;must be declared for linker (ld)
+	
+_start:	            ;tells linker entry point
+   mov	edx,len     ;message length
+   mov	ecx,msg     ;message to write
+   mov	ebx,1       ;file descriptor (stdout)
+   mov	eax,4       ;system call number (sys_write)
+   int	0x80        ;call kernel
+	
+   mov	eax,1       ;system call number (sys_exit)
+   int	0x80        ;call kernel
 
-mov eax,1 ; system call number (sys_exit)
-int 0x80 ; call kernel
-
-segment .data ;data segment
-msg db 'Hello world', 0xa ;our dear string 
-len equ $ - msg  ;lenght of our dear string
+section	.data
+msg db 'Hello, world!', 0xa  ;string to be printed
+len equ $ - msg     ;length of the string
