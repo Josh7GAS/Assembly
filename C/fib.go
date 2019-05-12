fb5cf9ac25e7ce81808bd26aafddd644d10b8f17
Josh7GAS <joshuaandre@gmail.com>
2019-05-12 20:44:27 -0300

    exercicio de exibição do valor de rax

---
+++ C/fib.go
@@ -0,0 +1,42 @@
+	.file	"fib.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"%d\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+.L4:
+	movl	$0, -12(%rbp)
+	movl	$1, -8(%rbp)
+.L3:
+	movl	-12(%rbp), %eax
+	movl	%eax, %esi
+	leaq	.LC0(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	-12(%rbp), %edx
+	movl	-8(%rbp), %eax
+	addl	%edx, %eax
+	movl	%eax, -4(%rbp)
+	movl	-8(%rbp), %eax
+	movl	%eax, -12(%rbp)
+	movl	-4(%rbp), %eax
+	movl	%eax, -8(%rbp)
+	cmpl	$254, -12(%rbp)
+	jg	.L4
+	jmp	.L3
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 7.4.0-1ubuntu1~18.04) 7.4.0"
+	.section	.note.GNU-stack,"",@progbits
