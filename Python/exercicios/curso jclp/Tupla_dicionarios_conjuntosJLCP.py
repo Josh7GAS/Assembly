9effba870e26892e56d2708a28bdf5287e1145ee
Joshua Sousa <joshua@nubeliu.com>
2019-07-12 15:13:45 -0300

    exercicos de python

---
+++ Python/exercicios/curso jclp/Tupla_dicionarios_conjuntosJLCP.py
@@ -0,0 +1,10 @@
+minha_tupla = ()
+meu_dicionario = {}
+meu_conjunto = set()
+
+minha_tupla = ('Robert', 'Pdero', 'Paulo')
+print(minha_tupla)
+print(minha_tupla[1])
+for nome in minha_tupla:
+    print(nome)
+print('')
--- Python/exercicios/curso jclp/lista2JLCP.py
+++ Python/exercicios/curso jclp/lista2JLCP.py
@@ -1,3 +1,19 @@
 nomes = ['José', 'Pedro', 'Paulo', 'Antonio']
 print(len(nomes))
-print('O nome informado é: ')
+print('O nome informado é: {}'.format(nomes[0]))
+print('O nome informado é: {}'.format(nomes[1]))
+
+for cada_nome in nomes:
+    print('O nome informado é: {}'.format(cada_nome))
+
+print('')
+for item in range(1, 11):
+    print(item)
+
+lista_numeros = []
+print(' ')
+for item in range(1, 101):
+    lista_numeros.append(item)
+
+print(len(lista_numeros))
+print(lista_numeros)
