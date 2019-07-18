ae38d2758386f493a9ec6429c2cc9f04d6c96221
Josh7GAS <joshuaandre@gmail.com>
2019-07-17 22:08:28 -0300

    adicionando openteste e mais exercicios em python

---
+++ Python/pratica/pratica1.py
@@ -0,0 +1,80 @@
+#####################################
+#### PART 6: EXERCISE REVIEW  #######
+#####################################
+
+# Time to review all the basic data types we learned! This should be a
+# relatively straight-forward and quick assignment.
+
+###############
+## Problem 1 ##
+###############
+
+# Given the string:
+s = 'django'
+
+# Use indexing to print out the following:
+# 'd'
+for index, proc in enumerate(s):
+    if proc == 'd':
+        print(index, proc)
+
+# 'o'
+for index, proc in enumerate(s):
+    if proc == 'o':
+        print(index, proc)
+# 'djan'
+for proc in s:
+    for esse in 'djan':
+        if esse == s:
+            var: str
+            var += esse
+            print(var)
+        else:continue
+# 'jan'
+
+# 'go'
+print(s[4:6])
+# Bonus: Use indexing to reverse the string
+
+
+###############
+## Problem 2 ##
+###############
+
+# Given this nested list:
+l = [3,7,[1,4,'hello']]
+# Reassign "hello" to be "goodbye"
+
+
+###############
+## Problem 3 ##
+###############
+
+# Using keys and indexing, grab the 'hello' from the following dictionaries:
+
+d1 = {'simple_key':'hello'}
+
+d2 = {'k1':{'k2':'hello'}}
+
+d3 = {'k1':[{'nest_key':['this is deep',['hello']]}]}
+
+
+###############
+## Problem 4 ##
+###############
+
+# Use a set to find the unique values of the list below:
+mylist = [1,1,1,1,1,2,2,2,2,3,3,3,3]
+
+
+###############
+## Problem 5 ##
+###############
+
+# You are given two variables:
+age = 4
+name = "Sammy"
+
+# Use print formatting to print the following string:
+"Hello my dog's name is Sammy and he is 4 years old"
+
