233eceb428a6a0b31a6d4ca5990f8d3e6565ea1f
Josh7GAS <joshuaandre@gmail.com>
2019-10-02 21:23:30 -0300

    movendo barco

--- Java/Stoya/src/Canoa/Rio.java
+++ Java/Stoya/src/Canoa/Rio.java
@@ -2,70 +2,49 @@ package Canoa;
 
 public class Rio {
 
-	boolean [] rio;
-	int tamanhoRio;
-	Canoa canoa;
+    boolean[] rio;
+    int tamanhoRio;
+    Canoa canoa;
 
+    public Rio(int tamanhoRio, Canoa canoa) {
+        super();
 
-	public Rio(int tamanhoRio, Canoa canoa) {
-		super();
+        this.rio = new boolean[tamanhoRio];
+        this.canoa = canoa;
 
-		this.rio = new boolean[tamanhoRio];
-		this.canoa = canoa;
+    }
 
-	}
+    public boolean[] getRio() {
+        return rio;
+    }
 
-	public boolean[] getRio() {
-		return rio;
-	}
+    public void setRio(boolean[] rio) {
+        this.rio = rio;
+    }
 
+    public int getTamanhoCanoa() {
+        return canoa.tamanhoCanoa;
+    }
 
-
-	public void setRio(boolean[] rio) {
-		this.rio = rio;
-	}
-
-	
-//		public boolean[] moverBarco(int x) {
-//			Canoa teste = new Canoa(4);
-//			if (x < teste.getCanoa().length|| x > teste.getCanoa().length) {
-//	
-//				System.out.println("Posição Invalida");
-//	
-//			}else {
-//	
-//				for (int count = x; count < teste.getCanoa().length ; count++) {
-//					getRio()[count]=true;
-//				}
-//	
-//			}return getRio();
-//	
-//		}
-//		
-
-	//	void receberTiro(int x) {
-	//		if (x<tamanho || x>tamanho) {
-	//			
-	//			System.out.println("Tiro invalido");
-	//			
-	//		}else{
-	//			
-	//			for (int count = 0; count < tamanho; count++) {
-	//				
-	//				if (rio[x]==false) {
-	//					
-	//					System.out.println("atirou na agua");
-	//					
-	//				}else{
-	//					
-	//					System.out.println("Acertou");
-	//					rio[x]=false;
-	//				}
-	//			}
-	//
-	//		}
-	//	}
-
+    public boolean moveBarco(int posicaoEscolhida) {
+        // 0 - tamahhoRio
+        //ou melhor=> tamanhoRio - tamanhoCanoa
+        if (posicaoEscolhida >= 0 && posicaoEscolhida < (tamanhoRio - canoa.tamanhoCanoa)) {
+            int contCanoa = 0;
+            for (int contRio = 0; contRio < tamanhoRio; contRio++) {
+                if (contRio >= posicaoEscolhida && contRio < (posicaoEscolhida + canoa.tamanhoCanoa)) {
+                    rio[contRio] = canoa.getCanoa()[contCanoa];
+                    contCanoa++;
+                }
+                else{
+                    rio[contRio] = false;
+                }
+            }
+            
+            return true;
+        }
+        
+        return false;
+    }
 
 }
-
