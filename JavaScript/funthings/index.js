2c52053dd88e367221e98fc62a05fbb8366b9d4d
Josh7GAS <joshuaandre@gmail.com>
2019-11-22 16:42:43 -0300

    bitTorrent client tutorial

---
+++ JavaScript/funthings/index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const fs = require('fs');
+const bencode = require('bencode');
+
+const dgram = require('dgram');
+const Buffer = require('buffer').Buffer;
+const urlParse = require('url').parse;
+
+const socket = dgram.createSocket("udp4");
+
+const myMsg = Buffer.from('hello?', 'utf8');
+
+socket.send(myMsg, 0, myMsg.length, url.port, url.host, ()=>{});
+
+socket.on('message', msg =>{
+    console.log('message', msg =>{
+        console.log('message is', msg);
+    })
+})
+const torrent = bencode.decode(fs.readFileSync('puppy.torrent'));
+console.log(torrent.toString('utf-8'));
