a35694b6a9fb46a90e934fc828e582da67a21672
Josh7GAS <joshuaandre@gmail.com>
2019-12-26 00:57:12 -0200

    New leassons

---
+++ Unity/Projects/CarPrototype/Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    public float speed = 5.0f;
+    public float turnSpeed;
+    public float horizontalInput;
+    public float fowardInput;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        fowardInput = Input.GetAxis("Vertical");
+        //Move the vehicle forward
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * fowardInput);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+    }
+}
