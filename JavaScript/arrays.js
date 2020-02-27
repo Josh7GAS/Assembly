9feaa48ac3b49a5b6a0f66f2c566ffa038ff4500
Josh7GAS <joshuaandre@gmail.com>
2020-02-26 22:14:07 -0300

    Adding a remove duplicate algorithm

--- JavaScript/arrays.js
+++ JavaScript/arrays.js
@@ -1,5 +1,22 @@
-let selectedColors = ['red', 'blue'];
-selectedColors[2]= 'green';
-console.log(selectedColors.length);
+// let selectedColors = ['red', 'blue'];
+// selectedColors[2]= 'green';
+// console.log(selectedColors.length);
 
 
+let names = ["jose", "ronaldo", "joao", "marcos", "arroz", "ronaldo", "arroz", "maria", "marcos", "joao"];
+//let unique = [];
+// let namesLen = names.namesLength;
+// let _temp;
+
+// names.sort();
+
+// for (let i = 0; i < namesLen; i++) {
+//     if (names[i] !== _temp) {
+//         unique.push(names[i]);
+//         _temp = names[i];
+//     }
+// }
+
+console.log([...new Set(names)]);
+
+//console.log(unique);
