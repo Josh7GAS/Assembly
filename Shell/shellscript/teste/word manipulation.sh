de9bf76501cce0447ba2d95d2c7c830495549fcf
root <root@Mach5.Mach5>
2020-05-30 01:03:34 -0300

    Script to get the auid and comm DONE

--- Shell/shellscript/teste/word manipulation.sh
+++ Shell/shellscript/teste/word manipulation.sh
@@ -1,18 +1,17 @@
 #/bin/bash
 COUNT=0;
 VALUE=0;
-NEW_OUTPUT=()
+VAR=0;
 COMM='comm="nom-http"';
 for AUX in ` grep -P -o '(?x)((?<=\Kauid=)|(?<=\Kcomm="nom))[\S]+' audit.log.1` 
 do
    MATCHVALUES[$COUNT]=$AUX; 
    #do manipulations here
-    COUNT=$(($COUNT+1)); 
+    COUNT=$(($COUNT+1));
    
-done 
-echo "Value of third element in my array : ${MATCHVALUES[@]} ";
- MATCHVALUES_LENGTH=${#MATCHVALUES[@]}
- 
+done
+#echo "Value of third element in my array : ${MATCHVALUES[@]} ";
+ MATCHVALUES_LENGTH=${#MATCHVALUES[@]};
 
 # for LOOKINSIDE in "${MATCHVALUES[@]}"; 
 # do
@@ -29,11 +28,22 @@ echo "Value of third element in my array : ${MATCHVALUES[@]} ";
 
 # echo "Value of third element in my array : ${NEW_OUTPUT[$COUNT_2]} ";
 # echo "NEW_OUTPUT_LENGTH = ${#NEW_OUTPUT[@]}";
-while [ $VALUE -le $MATCHVALUES_LENGTH ]; do
-    if [ ${MATCHVALUES[VALUE]} -eq $COMM ]
-       then            
-             NEW_OUTPUT[$VALUE]+=($MATCHVALUES[$VALUE]);
+
+while [ $VALUE -lt $MATCHVALUES_LENGTH ]; do
+    if [ ${MATCHVALUES[VALUE]} == $COMM ]; then       
+         NEW_OUTPUT[$VAR]+=${MATCHVALUES[$VALUE-1]};
+         VAR=$(($VAR+1));
+         NEW_OUTPUT[$VAR]+=${MATCHVALUES[$VALUE]};
+         VAR=$(($VAR+1));
          fi
          VALUE=$(($VALUE+1));
-         echo "NEW_OUTPUT_LENGTH = ${#NEW_OUTPUT[@]}";
 done
+echo 'count'$COUNT;
+echo 'matchvlues' $MATCHVALUES_LENGTH;
+echo 'value' $VALUE;
+echo "NEW_OUTPUT_LENGTH = ${#NEW_OUTPUT[@]}"; 
+echo ${NEW_OUTPUT[@]};
+# for show in ${NEW_OUTPUT[@]};
+# do
+#     echo $show
+#     done
