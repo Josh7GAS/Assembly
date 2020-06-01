1ec5b97cbe4cd23f5f62b5a2238b93f6f175d9b1
Josh7GAS <joshuaandre@gmail.com>
2020-05-31 21:04:40 -0300

    getting subent mask function WIP

--- Rust/CLI_tool_practice/subnet_calculator/src/main.rs
+++ Rust/CLI_tool_practice/subnet_calculator/src/main.rs
@@ -8,7 +8,8 @@ use structopt::StructOpt;
 fn main() {
     // let args = Cli::from_args();
     // let ip = &args.ip_adress;
-    binary_calculator()
+    //binary_calculator();
+    getting_subnet_mask();
 }
 
 fn binary_calculator() {
@@ -18,7 +19,7 @@ fn binary_calculator() {
 
     println!("We want to find = {}\n", num);
 
-    for walk in &arr {y
+    for walk in &arr {
         if walk <= &num {
             aux += walk;
             println!("adding:{}, current value is {} ", walk, aux);
@@ -27,14 +28,33 @@ fn binary_calculator() {
             }
             if aux > num {
                 aux -= walk;
-                println!(
-                    "descarting:{}",
-                    walk
-                );
+                println!("descarting:{}", walk);
             } else {
                 continue;
             }
         }
     }
     println!("\nthe calculator returned {}", aux);
+}
+
+fn getting_subnet_mask() {
+    let mut num = 0;
+    let bar = 27;
+    let bit_in_octets = 32;
+    let bits = vec![128, 64, 32, 16, 8, 4, 2, 1];
+    let mut octet = vec![0; bit_in_octets];
+
+    while num < bit_in_octets {
+        for put_in_octet in &bits {
+            if num >= bar {
+                octet[num] = 0;
+                num += 1;
+            } else {
+                octet[num] = *put_in_octet;
+                num += 1;
+            }
+        }
+    }
+    println!("{:?}", octet);
+    println!("{}", octet.len());
 }
