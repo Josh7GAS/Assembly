7f7017dd55d09d444f40f93ede034928cb6deea0
Josh7GAS <32309141+Josh7GAS@users.noreply.github.com>
2020-06-10 16:29:20 -0300

    Changing the file reading to parse on regex

--- Python/regexTest.py
+++ Python/regexTest.py
@@ -1,27 +1,26 @@
 import regex
-pattern = regex.compile('(?x)((?<=\Kauid=)|(?<=\Kcomm="nom))[\S]+')
+#pattern = regex.compile(r'(?x)((?<=\Kauid=)|(?<=\Kcomm="nom))[\S]+')
+pattern = (r'(?x)((?<=\Kauid=)|(?<=\Kcomm="nom))[\S]+')
 matchvalues = []
 new_output = []
+filename ="/home/joshua/Script/audit.log.1"
 comm = 'comm="nom-http"'
-i = 1
+i = 0
 
-with open('/home/joshua/Script/audit.log.1', 'r') as file:
-    for line in file:
-        for word in line.split():
-            matchvalues.append(word)
+with open(filename) as audit:
+    for line in audit:
+        if pattern in line:
+            print(line)
 
-            
 matchvalues_size = len(matchvalues)
+#print(matchvalues)
 
 while i < matchvalues_size:
     if matchvalues[i] == comm:
         new_output.append(matchvalues[i-1])
-        i+=1
         new_output.append(matchvalues[i])
         i+=1
     else:
         i+=1
         continue
-
-print(matchvalues_size)
-print(i)
+#print(new_output)
