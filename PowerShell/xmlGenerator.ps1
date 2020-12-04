6745adee8739e91ecbaa89608a7d7396400c2473
Joshua Sousa <Joshua.sousa@la.logicalis.com>
2020-12-04 12:06:42 -0300

    outputing the ip, mask, mask binary mode, number of subnets and number of hosts

--- PowerShell/xmlGenerator.ps1
+++ PowerShell/xmlGenerator.ps1
@@ -2,9 +2,9 @@
 $ip_addrees = $( Read-Host  "What is the IP Adreess, if have the mask also insert" )
 $bit_ip = 32
 $byte = 8
-$binarry_array = New-Object System.Collections.ArrayList
+$binarry_array = [Object[]]::new($bit_ip)
 
-
+#separating ip from mask
 foreach ($input in $ip_addrees) {
 
     #gettings the mask
@@ -14,15 +14,12 @@ foreach ($input in $ip_addrees) {
     #$ip1, $ip2, $ip3, $ip4 = $input.Split(".")
 
 }
-Write-Host $ip
-Write-Host $mask
-#getting the mask binary
-$binarys_on_octec = 1
-$number_of_hosts = 0
 
+#getting $number_of_enable_host and the mask binary form
+$binarys_on_octec = 0
 for ($count = 0; $count -lt $bit_ip; $count++) {
     if ($binarys_on_octec -eq $byte) {
-        $binarry_array[$count] += "."
+        $binarry_array[$count] = "."
         $binarys_on_octec = 1
     }
     else {
@@ -35,16 +32,32 @@ for ($count = 0; $count -lt $bit_ip; $count++) {
     }
     else {
         $binarry_array[$count] += 0
-        $number_of_hosts += 1
+        $number_of_enable_hosts += 1
     }
 }
 
-Write-Host $binarry_array
+#getting number_of_enable_network
+if ($mask -le 15) {
+
+    $number_of_enable_network = $mask - 8
+}
+elseif ($mask -le 23) {
+
+    $number_of_enable_network = $mask - 16
+}
+else {
+    $number_of_enable_network = $mask - 24
+}
+$number_of_enable_network = $mask - $avaible_host
+
 
 #getting subnets
-$subnets = [math]::Pow(2, $number_of_hosts)
+$subnets = [math]::Pow(2, $number_of_enable_network)
 #getting the Hosts
-$hosts = [math]::Pow(2, $number_of_hosts) - 2
+$hosts = [math]::Pow(2, $number_of_enable_hosts) - 2
 
-Write-Host $subnets
-Write-Host $hosts
+Write-Host "IP: "$ip
+Write-Host "Mask: "$mask
+Write-Host "Mask Binary mode: " $binarry_array
+Write-Host "Number of subnets: "$subnets
+Write-Host "Number of hosts: "$hosts
