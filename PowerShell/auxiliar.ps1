445b67ea7403aa6f873f8acd450bcf1e8e0746df
Josh7GAS <joshuaandre@gmail.com>
2021-01-06 17:27:29 -0300

    Outuput is corrct now

--- PowerShell/auxiliar.ps1
+++ PowerShell/auxiliar.ps1
@@ -3,15 +3,17 @@ $ip_addrees = $( Read-Host  "What is the IP Adreess, if have the mask also inser
 $bit_ip = 32
 $byte = 8
 $binarry_array = [Object[]]::new($bit_ip)
-$bound_of_octets_beg = New-Object System.Collections.ArrayList
+$bound_of_octets_beg = [System.Collections.ArrayList]::new()
+$bound_of_octets_beg1 = [System.Collections.ArrayList]::new()
 $bound_of_octets_end = New-Object System.Collections.ArrayList
+$bound_of_octets_end1 = New-Object System.Collections.ArrayList
 
 
 #separating ip from mask
 foreach ($input in $ip_addrees) {
 
     #gettings the mask
-    $ip, $mask = $input.Split("/")
+    $ip, $mask = $input.Split("/")  
 
     #getting the IP
     #$ip1, $ip2, $ip3, $ip4 = $input.Split(".")
@@ -81,8 +83,8 @@ for ($octet2 = 0; $octet2 -le 255; $octet2 += 1) {
     for ($octet3 = 0; $octet3 -le 255; $octet3 += 64) {
        
         
-        $bound_of_octets_beg1 += -join "<IPRange><Start> 10." + $octet2.ToString() + "." + $octet3.ToString(), ". 1"
-        $bound_of_octets_beg += -join $bound_of_octets_beg1
+        $bound_of_octets_beg1 += "<IPRange><Start>10." + $octet2.ToString() + "." + $octet3.ToString() + ".1"
+        #$bound_of_octets_beg +=  $bound_of_octets_beg1
     
     
         # if ($octet2 -eq 255) {$writing_the_xml_file += -join $bound_of_octets
@@ -95,20 +97,20 @@ for ($octet2 = 0; $octet2 -le 255; $octet2 += 1) {
 for ($octe2end = 0; $octe2end -le 255; $octe2end += 1) {
     for ($octet3end = 63; $octet3end -le 255; $octet3end += 64) {
         
-        $bound_of_octets_end1 += "</Start><End> 10."+ $octe2end+ "."+ $octet3end.ToString(), ". 254 </End></IpRange>"
-        $bound_of_octets_end += -join $bound_of_octets_end1
+        $bound_of_octets_end1 += "</Start><End>10." + $octe2end + "." + $octet3end.ToString()+ ".254</End></IPRange>"
+       # $bound_of_octets_end += -join $bound_of_octets_end1
     }
 }
 
 $writing_the_xml_file = New-Object System.Collections.ArrayList
 
-foreach ($index in 0..$bound_of_octets_beg.Length) {
+foreach ($index in 0..$bound_of_octets_beg1.Length) {
     $writing_the_xml_file += " "
 }
 
 #colocar um for looping para unir as listas
-for ($couting_both_list = 0; $couting_both_list -lt $bound_of_octets_beg.Length; $couting_both_list++) {
-    $writing_the_xml_file[$couting_both_list] = $bound_of_octets_beg[$couting_both_list] + $bound_of_octets_end[$couting_both_list]
+for ($couting_both_list = 0; $couting_both_list -lt $bound_of_octets_beg1.Length; $couting_both_list++) {
+    $writing_the_xml_file[$couting_both_list] = $bound_of_octets_beg1[$couting_both_list] + $bound_of_octets_end1[$couting_both_list]
 
 }
 
