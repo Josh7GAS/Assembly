eb67c4cff73055b2004bc5d44824e55c7c9daea9
Josh7GAS <joshuaandre@gmail.com>
2021-02-17 18:27:20 -0300

    updates

--- Rust/Rust_OS/rust_os/target/rls/debug/deps/rust_os-ee166c6692081907.d
+++
@@ -1,5 +0,0 @@
-d:\Users\re92331z\github.com\Josh7GAS\Oficina\Rust\Rust_OS\rust_os\target\rls\debug\deps\rust_os-ee166c6692081907.rmeta: src\main.rs
-
-d:\Users\re92331z\github.com\Josh7GAS\Oficina\Rust\Rust_OS\rust_os\target\rls\debug\deps\rust_os-ee166c6692081907.d: src\main.rs
-
-src\main.rs:
--- Rust/Rust_OS/rust_os/target/thumbv7em-none-eabihf/debug/rust_os.d
+++
@@ -1,1 +0,0 @@
-D:\Users\re92331z\github.com\Josh7GAS\Oficina\Rust\Rust_OS\rust_os\target\thumbv7em-none-eabihf\debug\rust_os: D:\Users\re92331z\github.com\Josh7GAS\Oficina\Rust\Rust_OS\rust_os\src\main.rs
